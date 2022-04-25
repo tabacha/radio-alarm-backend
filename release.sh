2c11e27de270d30700a9a93a0c833f443e004412
Sven Anders <sven@anders-hamburg.de>
2022-04-25 07:13:03 +0200

    Build script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VERSION=$(dpkg-parsechangelog -S Version)
+dch -D stable -r release
+git commit -m "New Version $VERSION" debian/changelog
+git tag v$VERSION
+git push origin v$VERSION
+dch -i ""
+NEWVERSION=$(dpkg-parsechangelog -S Version)
+git commit -m "Starting with version $NEWVERRSION"
