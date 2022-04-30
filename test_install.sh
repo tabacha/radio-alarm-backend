f5ad90c744577e7d81c6381946a4f0d514a756eb
Sven Anders <sven@anders-hamburg.de>
2022-04-30 08:25:29 +0200

    Helper script for install

--- test_install.sh
+++ test_install.sh
@@ -2,6 +2,6 @@
 VER=$(dpkg-parsechangelog -S Version)
 RDEB="radio-alarm-backend_${VER}_all.deb"
 dpkg-buildpackage --no-sign -rfakeroot && \
-    dpkg -c ${RDEB} && \
+    dpkg -c ../${RDEB} && \
     scp ../${RDEB} root@radio.anders.hamburg:. && \
     ssh  root@radio.anders.hamburg dpkg -i ${RDEB}
