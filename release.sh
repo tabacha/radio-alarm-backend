add08be98c8d6c664792c99378522accb4d6d315
Sven Anders <sven@anders-hamburg.de>
2022-04-30 10:45:31 +0200

    Package Error fix

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ dch -D stable -r release
 git commit -m "New Version $VERSION" debian/changelog
 git tag v$VERSION
 git push origin v$VERSION
-dch -i ""
+dch -i "change this"
 NEWVERSION=$(dpkg-parsechangelog -S Version)
-git commit -m "Starting with version $NEWVERRSION"
+git commit -m "Starting with version $NEWVERRSION" debian/changelog
