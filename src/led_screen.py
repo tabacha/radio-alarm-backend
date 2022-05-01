8fd7e0c2314a55026d24e7bd0bfdb05564fcf4d0
Sven Anders <sven@anders-hamburg.de>
2022-05-01 09:34:39 +0200

    Screens in extra file+ Zustand laden und speichern

---
+++ src/led_screen.py
@@ -0,0 +1,243 @@
+import gpiodev
+from math import floor
+from datetime import datetime
+WOCHENTAG=['Mo','Di','Mi','Do','Fr','Sa','So']
+
+class AbstractMenuEntry():
+
+    def getTitle(self)->str:
+        pass
+    def run(self):
+        pass
+
+class StaticTitleMenuEntry(AbstractMenuEntry):
+
+    def __init__(self,title:str,action,param=None):
+        self.myaction=action
+        self.title=title
+        self.param=param
+    def getTitle(self)->str:
+        return self.title
+
+    def run(self):
+        if self.param==None:
+            self.myaction()
+        else:
+            self.myaction(self.param)
+
+class OnOffMenuEntry(AbstractMenuEntry):
+
+    def __init__(self,title:str,action=None, status=False,param=None):
+        self.myaction=action
+        self.title=title
+        self.status=status
+        self.param=param
+
+    def getTitle(self)->str:
+        if (self.status):
+            return "[x] %s" % self.title
+        else:
+            return "[ ] %s" % self.title
+
+    def run(self):
+        self.status=not(self.status)
+        if self.myaction:
+            if self.param==None:
+                self.myaction(self.status)
+            else:
+                self.myaction(self.param, self.status)
+
+
+    def getStatus(self)->bool:
+        return self.status
+
+class AbstractGpioScreen():
+
+    def __init__(self, gpdev:gpiodev.GPIOBaseDev):
+        self.gpdev=gpdev
+        self.is_running=True
+        self.timeout_in_secs=120
+
+    def buttonFunc(self):
+        pass
+
+    def leftFunc(self):
+        pass
+
+    def rightFunc(self):
+        pass
+
+    def timeoutFunc(self):
+        pass
+
+    def printScreen(self):
+        pass
+
+
+    def exitFunc(self):
+        self.is_running=False
+
+    def running(self)->bool:
+        return self.is_running
+
+    def run(self):
+        self.is_running=True
+        self.gpdev.clear()
+        while self.running():
+            self.printScreen()
+            actionFunc= {
+                gpiodev.GPIO_INPUT.BUTTON: self.buttonFunc,
+                gpiodev.GPIO_INPUT.LEFT: self.leftFunc,
+                gpiodev.GPIO_INPUT.RIGHT: self.rightFunc,
+                gpiodev.GPIO_INPUT.TIMEOUT: self.timeoutFunc,
+            }
+            k=self.gpdev.wait_for_gpio_event(self.timeout_in_secs)
+            actionFunc[k]()
+
+class MenuEntryScreen(AbstractGpioScreen):
+
+    def __init__(self, gpdev:gpiodev.GPIOBaseDev, choices:list, idx:int=0, add_back_entry=True):
+        if idx>=len(choices):
+            raise ValueError('idx out of range')
+        self.is_running=True
+        self.idx=idx
+        if idx>2:
+            self.offset=self.idx-2
+        else:
+            self.offset=0
+        self.choices=choices
+        self.add_back_entry=add_back_entry
+        if (add_back_entry):
+            self.choices.append( StaticTitleMenuEntry('zurück',self.exitFunc))
+        super().__init__(gpdev)
+
+    def buttonFunc(self):
+        if (self.add_back_entry==False):
+            self.exitFunc()
+        self.choices[self.idx].run()
+
+    def leftFunc(self):
+        if self.idx>0:
+            self.idx-=1
+            if self.idx>2 and self.idx==len(self.choices)-1:
+                self.offset=self.idx-3
+            elif self.idx>1 and self.idx==len(self.choices)-2:
+                self.offset=self.idx-2
+            elif self.idx>1:
+                self.offset=self.idx-1
+            else:
+                self.offset=0
+
+    def rightFunc(self):
+        if self.idx+1<len(self.choices):
+            self.idx+=1
+            if self.idx>2 and self.idx==len(self.choices)-1:
+                self.offset=self.idx-3
+            elif self.idx>2:
+                self.offset=self.idx-2
+            else:
+                self.offset=0
+
+    def timeoutFunc(self):
+        self.exitFunc()
+
+    def printScreen(self):
+        self.gpdev.clear()
+        for i in range(0,self.gpdev.get_number_of_lines()):
+            if i+self.offset<len(self.choices):
+                if (i+self.offset)==self.idx:
+                    chooser=self.gpdev.get_pfeil_rechts()+' '
+                else:
+                    chooser='  '
+                self.gpdev.write_display_line(i+1,chooser+self.choices[i+self.offset].getTitle())
+
+class ValueScreen(AbstractGpioScreen):
+    def __init__(self, gpdev:gpiodev.GPIOBaseDev, title:str, changeValueFunc, value:int, maxValue=100, suffix="%"):
+        self.title=title
+        self.maxValue=maxValue
+        self.value=value
+        self.suffix=suffix
+        self.changeValueFunc=changeValueFunc
+        super().__init__(gpdev)
+
+    def buttonFunc(self):
+        self.exitFunc()
+
+    def leftFunc(self):
+        if self.value>0:
+            self.value-=1
+            self.changeValueFunc(self.value)
+
+    def rightFunc(self):
+        if self.value<self.maxValue:
+            self.value+=1
+            self.changeValueFunc(self.value)
+
+    def printScreen(self):
+        self.gpdev.clear()
+        self.gpdev.write_display_line(1,"%s %d %s" %(self.title,self.value,self.suffix))
+        self.gpdev.write_display_line(2,"")
+        self.gpdev.write_display_line(3,"")
+        numFullChars=floor(20*(self.value/self.maxValue))
+        lastChar=floor((100*(self.value/self.maxValue)) % (100/20))
+        str=""
+        for i in range(0,numFullChars):
+            str=str+chr(5)
+        if numFullChars<20:
+            str=str+chr(lastChar)
+            while len(str)<20:
+                str=str+chr(0)
+        self.gpdev.write_display_line(4,str)
+
+class MainScreen(AbstractGpioScreen):
+    def __init__(self, gpdev:gpiodev.GPIOBaseDev):
+        super().__init__(gpdev=gpdev)
+        self.timeout_in_secs=0.5
+        self.night_mode_off_time_secs=60
+        self.displayOn=True
+        self.last_input_time=datetime.now()
+
+    def buttonFunc(self):
+        if self.isDisplayOn(datetime.now()):
+            self.exitFunc()
+        else:
+            self.last_input_time=datetime.now()
+
+    def leftFunc(self):
+        self.last_input_time=datetime.now()
+
+    def rightFunc(self):
+        self.last_input_time=datetime.now()
+
+    def isNightMode(self,now:datetime):
+        if now.hour<6:
+            return True
+        if now.hour==6 and now.minute<10:
+            return True
+        if now.hour>22:
+            return True
+        return False
+
+    def isDisplayOn(self,now:datetime):
+        if self.isNightMode(now):
+            return (now.timestamp()-self.last_input_time.timestamp())<self.night_mode_off_time_secs
+        else:
+            return True
+
+    def printScreen(self):
+        # datetime object containing current date and time
+        now = datetime.now()
+        if self.isDisplayOn(now):
+            self.gpdev.backlight(True)
+            weekday=WOCHENTAG[now.weekday()]
+            if (now.second==0):
+                self.gpdev.clear()
+            t_string = now.strftime("%H:%M:%S")
+            d_string = now.strftime("%d.%m.%y")
+            self.gpdev.write_display_line(1,"%s %s %s" %(t_string, weekday, d_string))
+            naechsterWecker='06:10 Mo. 10.05.22'
+            self.gpdev.write_display_line(2,"Nächster Wecker"+chr(6))
+            self.gpdev.write_display_line(3,"%s" % (naechsterWecker))
+            self.gpdev.write_display_line(4,"in 3h 10m + 55 Tagen")
+        else:
+            self.gpdev.backlight(False)
--- src/radio-display.py
+++ src/radio-display.py
@@ -5,261 +5,64 @@ from time import sleep
 import argparse
 from datetime import datetime
 from math import floor
-WOCHENTAG=['Mo','Di','Mi','Do','Fr','Sa','So']
+from led_screen import StaticTitleMenuEntry, MenuEntryScreen, OnOffMenuEntry, ValueScreen, MainScreen
+import re
+from pprint import pprint
 
-class AbstractMenuEntry():
+WOCHENTAG = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']
+WT_DATA = {"1": {"name": "Weckzeit 1", "time": "5:42", "active": True, "monday": True,
+                 "tuesday": True, "wednesday": True, "thursday": True, "friday": True,
+                 "saturday": False, "sunday": False, "once": False, "free": False, "not_free": False},
+           "2": {"name": "Weckzeit 2", "time": "6:42", "active": False, "monday": True,
+                 "tuesday": True, "wednesday": True, "thursday": True, "friday": True,
+                 "saturday": False, "sunday": False, "once": False, "free": False, "not_free": False},
+           "3": {"name": "Weckzeit 3", "time": "23:42", "active": False, "monday": True,
+                 "tuesday": True, "wednesday": True, "thursday": True, "friday": True,
+                 "saturday": False, "sunday": False, "once": False, "free": False, "not_free": False}}
 
-    def getTitle(self)->str:
-        pass
-    def run(self):
-        pass
+def getSender():
+    return ['NDR 2', 'NDR 90,3', 'Radio Hamburg']
 
-class StaticTitleMenuEntry(AbstractMenuEntry):
+def setSender(sender:str):
+    print(sender)
+    sleep(3)
 
-    def __init__(self,title:str,action,param=None):
-        self.myaction=action
-        self.title=title
-        self.param=param
-    def getTitle(self)->str:
-        return self.title
+def getLautstaerke()->int:
+    return 12
 
-    def run(self):
-        if self.param==None:
-            self.myaction()
-        else:
-            self.myaction(self.param)
+def setLautstaerke(vol:int):
+    print(vol)
+    sleep(3)
 
-class OnOffMenuEntry(AbstractMenuEntry):
+def isRadioOn()->bool:
+    return True
 
-    def __init__(self,title:str,action=None, status=False,param=None):
-        self.myaction=action
-        self.title=title
-        self.status=status
-        self.param=param
+def schalteRadioAnAus(on:bool):
+    print(on)
+    sleep(3)
 
-    def getTitle(self)->str:
-        if (self.status):
-            return "[x] %s" % self.title
-        else:
-            return "[ ] %s" % self.title
+def getWackeupTime(idx:int):
+    return WT_DATA["%d" % idx]
 
-    def run(self):
-        self.status=not(self.status)
-        if self.myaction:
-            if self.param==None:
-                self.myaction()
-            else:
-                self.myaction(self.param)
-
-
-    def getStatus(self)->bool:
-        return self.status
-
-class AbstractGpioScreen():
-
-    def __init__(self, gpdev:gpiodev.GPIOBaseDev):
-        self.gpdev=gpdev
-        self.is_running=True
-        self.timeout_in_secs=120
-
-    def buttonFunc(self):
-        pass
-
-    def leftFunc(self):
-        pass
-
-    def rightFunc(self):
-        pass
-
-    def timeoutFunc(self):
-        pass
-
-    def printScreen(self):
-        pass
-
-
-    def exitFunc(self):
-        self.is_running=False
-
-    def running(self)->bool:
-        return self.is_running
-
-    def run(self):
-        self.is_running=True
-        self.gpdev.clear()
-        while self.running():
-            self.printScreen()
-            actionFunc= {
-                gpiodev.GPIO_INPUT.BUTTON: self.buttonFunc,
-                gpiodev.GPIO_INPUT.LEFT: self.leftFunc,
-                gpiodev.GPIO_INPUT.RIGHT: self.rightFunc,
-                gpiodev.GPIO_INPUT.TIMEOUT: self.timeoutFunc,
-            }
-            k=self.gpdev.wait_for_gpio_event(self.timeout_in_secs)
-            actionFunc[k]()
-
-class MenuEntryScreen(AbstractGpioScreen):
-
-    def __init__(self, gpdev:gpiodev.GPIOBaseDev, choices:list, idx:int=0, add_back_entry=True):
-        if idx>=len(choices):
-            raise ValueError('idx out of range')
-        self.is_running=True
-        self.idx=idx
-        if idx>2:
-            self.offset=self.idx-2
-        else:
-            self.offset=0
-        self.choices=choices
-        self.add_back_entry=add_back_entry
-        if (add_back_entry):
-            self.choices.append( StaticTitleMenuEntry('zurück',self.exitFunc))
-        super().__init__(gpdev)
-
-    def buttonFunc(self):
-        if (self.add_back_entry==False):
-            self.exitFunc()
-        self.choices[self.idx].run()
-
-    def leftFunc(self):
-        if self.idx>0:
-            self.idx-=1
-            if self.idx>2 and self.idx==len(self.choices)-1:
-                self.offset=self.idx-3
-            elif self.idx>1 and self.idx==len(self.choices)-2:
-                self.offset=self.idx-2
-            elif self.idx>1:
-                self.offset=self.idx-1
-            else:
-                self.offset=0
-
-    def rightFunc(self):
-        if self.idx+1<len(self.choices):
-            self.idx+=1
-            if self.idx>2 and self.idx==len(self.choices)-1:
-                self.offset=self.idx-3
-            elif self.idx>2:
-                self.offset=self.idx-2
-            else:
-                self.offset=0
-
-    def timeoutFunc(self):
-        self.exitFunc()
-
-    def printScreen(self):
-        self.gpdev.clear()
-        for i in range(0,self.gpdev.get_number_of_lines()):
-            if i+self.offset<len(self.choices):
-                if (i+self.offset)==self.idx:
-                    chooser=self.gpdev.get_pfeil_rechts()+' '
-                else:
-                    chooser='  '
-                self.gpdev.write_display_line(i+1,chooser+self.choices[i+self.offset].getTitle())
-
-class ValueScreen(AbstractGpioScreen):
-    def __init__(self, gpdev:gpiodev.GPIOBaseDev, title:str, value:int, maxValue=100, suffix="%"):
-        self.title=title
-        self.maxValue=maxValue
-        self.value=value
-        self.suffix=suffix
-        super().__init__(gpdev)
-
-    def buttonFunc(self):
-        self.exitFunc()
-
-    def leftFunc(self):
-        if self.value>0:
-            self.value-=1
-
-    def rightFunc(self):
-        if self.value<self.maxValue:
-            self.value+=1
-
-    def printScreen(self):
-        self.gpdev.clear()
-        self.gpdev.write_display_line(1,"%s %d %s" %(self.title,self.value,self.suffix))
-        self.gpdev.write_display_line(2,"")
-        self.gpdev.write_display_line(3,"")
-        numFullChars=floor(20*(self.value/self.maxValue))
-        lastChar=floor((100*(self.value/self.maxValue)) % (100/20))
-        str=""
-        for i in range(0,numFullChars):
-            str=str+chr(5)
-        if numFullChars<20:
-            str=str+chr(lastChar)
-            while len(str)<20:
-                str=str+chr(0)
-        self.gpdev.write_display_line(4,str)
-
-class MainScreen(AbstractGpioScreen):
-    def __init__(self, gpdev:gpiodev.GPIOBaseDev):
-        super().__init__(gpdev=gpdev)
-        self.timeout_in_secs=0.5
-        self.night_mode_off_time_secs=60
-        self.displayOn=True
-        self.last_input_time=datetime.now()
-
-    def buttonFunc(self):
-        if self.isDisplayOn(datetime.now()):
-            self.exitFunc()
-        else:
-            self.last_input_time=datetime.now()
-
-    def leftFunc(self):
-        self.last_input_time=datetime.now()
-
-    def rightFunc(self):
-        self.last_input_time=datetime.now()
-
-    def isNightMode(self,now:datetime):
-        if now.hour<6:
-            return True
-        if now.hour==6 and now.minute<10:
-            return True
-        if now.hour>22:
-            return True
-        return False
-
-    def isDisplayOn(self,now:datetime):
-        if self.isNightMode(now):
-            return (now.timestamp()-self.last_input_time.timestamp())<self.night_mode_off_time_secs
-        else:
-            return True
-
-    def printScreen(self):
-        # datetime object containing current date and time
-        now = datetime.now()
-        if self.isDisplayOn(now):
-            self.gpdev.backlight(True)
-            weekday=WOCHENTAG[now.weekday()]
-            if (now.second==0):
-                self.gpdev.clear()
-            t_string = now.strftime("%H:%M:%S")
-            d_string = now.strftime("%d.%m.%y")
-            self.gpdev.write_display_line(1,"%s %s %s" %(t_string, weekday, d_string))
-            naechsterWecker='06:10 Mo. 10.05.22'
-            self.gpdev.write_display_line(2,"Nächster Wecker"+chr(6))
-            self.gpdev.write_display_line(3,"%s" % (naechsterWecker))
-            self.gpdev.write_display_line(4,"in 3h 10m + 55 Tagen")
-        else:
-            self.gpdev.backlight(False)
+def setWackeupTime(idx:int,wt):
+    print(idx)
+    pprint(wt)
+    sleep(6)
 
 def doNothing():
     pass
 
-def setSender(sender):
-    print(sender)
-    sleep(3)
-
-def saveEinmalig(wzId:int):
-    pass
-
 def createUhrzeitMenu(dev, wzId:int):
-    h=6
-    dm=4
-    m=2
+    wt=getWackeupTime(wzId)
+    res=re.search(r"^(\d+):(\d)(\d)$", wt['time'])
+    h=int(res.group(1))
+    dm=int(res.group(2))
+    m=int(res.group(3))
     def setzeM(new_m:int):
-        nonlocal m
+        nonlocal m,dm,h
         m=new_m
+        wt['time']="%d:%d%d" % (h,dm,m)
+        setWackeupTime(wzId,wt)
     def setzeDM(new_dm:int):
         nonlocal h,dm
         dm=new_dm
@@ -272,7 +75,6 @@ def createUhrzeitMenu(dev, wzId:int):
     def setzeH(new_h:int):
         nonlocal h,dm
         print(new_h)
-        sleep(2)
         h=new_h
         dm_arr=[]
         for i in range(0,6):
@@ -286,44 +88,71 @@ def createUhrzeitMenu(dev, wzId:int):
     hMenu=MenuEntryScreen(dev,h_arr, idx=h,add_back_entry=False)
     return hMenu
 
+WZ_I18N={
+    'Montag':'monday',
+    'Dienstag': 'tuesday',
+    'Mittwoch': 'wednesday',
+    'Donnerstag': 'thursday',
+    'Freitag': 'friday',
+    'Samstag': 'saturday',
+    'Sonntag': 'sunday',
+    '+ an fr. T': 'free',
+    'Nicht fr. T.': 'not_free'
+}
 
 def createWeckzeitMenu(wzId:int,dev):
-    def saveWzTag(tag):
-        nonlocal wzId
-        print("%s %d" %(tag,wzId))
-        sleep(1)
+    wt=getWackeupTime(wzId)
+    def toggleActive():
+        nonlocal wt, wzId
+        wt['active']!=wt['active']
+        setWackeupTime(wzId,wt)
+
+    def saveWzTag(tag,status):
+        nonlocal wt, wzId
+        wt[tag]=status
+        setWackeupTime(wzId,wt)
+
+    def saveEinmalig():
+        nonlocal wzId,wt
+        wt['once']=True
+        for day in ["monday","tuesday","wednesday","thursday","friday",
+                 "saturday", "sunday", "free", "not_free"]:
+            wt[day]=False
+        setWackeupTime(wzId,wt)
+
     uhrMenu=createUhrzeitMenu(dev,wzId)
     tag_arr=[]
-    for tag in ['Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag','Sonntag']:
-        tag_arr.append(OnOffMenuEntry(tag, saveWzTag, status=True,param=tag))
-    for tag in ['Samstag','Sonntag']:
-        tag_arr.append(OnOffMenuEntry(tag, saveWzTag, status=False,param=tag))
+    for tag in WZ_I18N.keys():
+        day=WZ_I18N[tag]
+        tag_arr.append(OnOffMenuEntry(tag, saveWzTag, status=wt[day],param=day))
 
-    tag_arr.append(OnOffMenuEntry('+ an fr. T', saveWzTag, status=False,param='frei'))
-    tag_arr.append(OnOffMenuEntry('Nicht fr. T.', saveWzTag, status=False,param='Nicht_frei'))
     widerkehMenu=MenuEntryScreen(dev,tag_arr)
+    if wt['once']:
+        wtEinm=0
+    else:
+        wtEinm=1
+
     einMaligRegelmMenu=MenuEntryScreen(dev,[
-            StaticTitleMenuEntry('Einmalig',lambda: saveEinmalig(wzId)),
+            StaticTitleMenuEntry('Einmalig',saveEinmalig),
             StaticTitleMenuEntry('Wiederkehrend', widerkehMenu.run)
-            ], add_back_entry=False
+            ], add_back_entry=False,idx=wtEinm
         )
     wzMenu=MenuEntryScreen(dev,[
             StaticTitleMenuEntry('Zeit',uhrMenu.run),
             StaticTitleMenuEntry('Tage', einMaligRegelmMenu.run),
-            OnOffMenuEntry('Aktiv',lambda: setSender('aktiv %d' % wzId)),
+            OnOffMenuEntry('Aktiv',toggleActive,status=wt['active']),
         ])
     return wzMenu
 
 def runSenderMenu(dev):
-    senderMenu=MenuEntryScreen(dev,[
-        StaticTitleMenuEntry('NDR 2',lambda: setSender('NDR2')),
-        StaticTitleMenuEntry('NDR 90,3',lambda: setSender('NDR903')),
-        StaticTitleMenuEntry('Radio Hamburg',lambda: setSender('RHH')),
-    ])
+    menus=[]
+    for sender in getSender():
+        menus.append(StaticTitleMenuEntry(sender, setSender, param=sender))
+    senderMenu=MenuEntryScreen(dev,menus)
     senderMenu.run()
 
 def mainMenu(dev):
-    volScreen=ValueScreen(dev,'Lautstärke',value=12)
+    volScreen=ValueScreen(dev,'Lautstärke',value=12,changeValueFunc=setLautstaerke)
     wzAuswahlMenuEntries=[]
     for wzId in range(1,4):
         wzMenu=createWeckzeitMenu(wzId,dev)
@@ -335,7 +164,7 @@ def mainMenu(dev):
         StaticTitleMenuEntry('Lautstärke',volScreen.run),
         StaticTitleMenuEntry('Sender',lambda: runSenderMenu(dev)),
         StaticTitleMenuEntry('Weckzeit',wzAuwahlMenu.run),
-        OnOffMenuEntry('Radio an/aus', doNothing),
+        OnOffMenuEntry('Radio an/aus', schalteRadioAnAus, status=isRadioOn()),
     ]
     m=MenuEntryScreen(dev,menuList)
     m.run()
